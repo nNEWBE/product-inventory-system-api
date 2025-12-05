4cf1fd2b267610ace3791a9efd1c5736e799c0f9
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2025-12-06 00:33:14 +0600

    project initialized

---
+++ src/main/java/com/example/product_inventory_system_api/ProductInventorySystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_inventory_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInventorySystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInventorySystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/product_inventory_system_api/ProductInventorySystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.product_inventory_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventorySystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
